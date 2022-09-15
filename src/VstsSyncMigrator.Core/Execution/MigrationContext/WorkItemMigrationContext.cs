8e5f0a6217aaa0fada645b1fa1530675fa41f7d5
Ove Bastiansen <ovebastiansen@outlook.com>
2022-09-15 08:05:57 +0200

    string != string becomes true even if they are the same in some cases. string.equals(string) seems better

--- src/VstsSyncMigrator.Core/Execution/MigrationContext/WorkItemMigrationContext.cs
+++ src/VstsSyncMigrator.Core/Execution/MigrationContext/WorkItemMigrationContext.cs
@@ -377,8 +377,15 @@ namespace VstsSyncMigrator.Engine
 
             foreach (Field f in oldWorkItem.Fields)
             {
-                if (newWorkItem.Fields.Contains(f.ReferenceName) && !_ignore.Contains(f.ReferenceName) && (!newWorkItem.Fields[f.ReferenceName].IsChangedInRevision || newWorkItem.Fields[f.ReferenceName].IsEditable) && oldWorkItem.Fields[f.ReferenceName].Value != newWorkItem.Fields[f.ReferenceName].Value)
+                if (newWorkItem.Fields.Contains(f.ReferenceName) &&
+                    !_ignore.Contains(f.ReferenceName) &&
+                    (!newWorkItem.Fields[f.ReferenceName].IsChangedInRevision || newWorkItem.Fields[f.ReferenceName].IsEditable) &&
+                    oldWorkItem.Fields[f.ReferenceName].Value != newWorkItem.Fields[f.ReferenceName].Value)
                 {
+                    if (oldWorkItem.Fields[f.ReferenceName].Value != null && oldWorkItem.Fields[f.ReferenceName].Value.Equals(newWorkItem.Fields[f.ReferenceName].Value))
+                    {
+                        continue;
+                    }
                     Log.LogDebug("PopulateWorkItem:FieldUpdate: {ReferenceName} | Old:{OldReferenceValue} New:{NewReferenceValue}", f.ReferenceName, oldWorkItem.Fields[f.ReferenceName].Value, newWorkItem.Fields[f.ReferenceName].Value);
                     newWorkItem.Fields[f.ReferenceName].Value = oldWorkItem.Fields[f.ReferenceName].Value;
                 }
